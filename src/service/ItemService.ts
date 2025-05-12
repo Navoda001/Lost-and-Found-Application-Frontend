93f862a82edb1f115429b18b204d7d05e4cbcf7c
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-12 16:06:53 +0530

    create ItemService

---
+++ src/service/ItemService.ts
@@ -0,0 +1,53 @@
+import axios from 'axios'
+
+const baseURL = "http://localhost:8082/trackitem/api/v1/items"
+
+
+const AddItem = async (item: any) => {
+  console.log("Item data:", item);
+  try{
+    const response = await axios.post(baseURL, item);  // Don't manually set Content-Type header
+  return response;
+  }catch(error){
+    console.error("Failed to add the item",error);
+    throw error
+  }
+};
+
+const GetItemById = async (itemId:string) => {
+   //get the items
+      try{
+        const response = await  axios.get(  `${baseURL}?itemId=${itemId}`)
+        return response.data
+      }catch(error){
+        console.error("Failed to get the data",error);
+        throw error
+      }
+}
+
+const GetAllItems = async () => {
+   //get the items
+      try{
+        const response = await  axios.get(`${baseURL}/getAllItems`)
+        return response.data
+      }catch(error){
+        console.error("Failed to get the data",error);
+        throw error
+      }
+}
+
+const DeleteItem = async(itemId :String) =>{
+  try{
+    console.log(itemId)
+    const response  =  await axios.delete(
+        `${baseURL}?itemId=${itemId}`);
+    console.log(response.data)
+    return response.data;
+    
+  }catch(error){
+      console.error("Failed to delete the data",error);
+      throw error
+  }
+}
+
+export{GetAllItems,AddItem,GetItemById,DeleteItem}
