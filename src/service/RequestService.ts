fc7cae3d40489af49764cdca624de169c15ec3d2
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-16 23:45:55 +0530

    update

--- src/service/RequestService.ts
+++ src/service/RequestService.ts
@@ -13,4 +13,37 @@ const AddRequest = async (request: any) => {
   }
 };
 
-export{AddRequest}
+const GetRequestById = async (requestId:string) => {
+   //get the items
+      try{
+        const response = await  axios.get(  `${baseURL}?requestId=${requestId}`)
+        return response.data
+      }catch(error){
+        console.error("Failed to get the data",error);
+        throw error
+      }
+}
+
+const GetAllRequestsByIdItemId = async (itemId:string) => {
+   //get the items
+      try{
+        const response = await  axios.get(  `${baseURL}/getRequestsByItemId?itemId=${itemId}`)
+        return response.data
+      }catch(error){
+        console.error("Failed to get the data",error);
+        throw error
+      }
+}
+
+const GetAllRequestItems = async () => {
+   //get the items
+      try{
+        const response = await  axios.get(`${baseURL}/getAllItems`)
+        return response.data
+      }catch(error){
+        console.error("Failed to get the data",error);
+        throw error
+      }
+}
+
+export{AddRequest,GetRequestById,GetAllRequestItems,GetAllRequestsByIdItemId}
