f0fdf26998cc1f4b25cf1ec987d79644501e98f8
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-20 19:34:30 +0530

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import ItemConsole from "./components/Item/ItemConsole";
+import ItemConsole from './components/Item/ItemConsole';
 import RequestConsole from './components/Request/RequestConsole';
+import SignUp from './components/Signup';
+import Login from './components/Login';
+import Home from './components/Home';
+
+
+function AppWrapper() {
+  const location = useLocation();
+
+  // Define routes where NavBar should NOT be shown
+  const hideNavbarRoutes = ['/signup' , '/login'];
+
+  const showNavbar = !hideNavbarRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {showNavbar && <NavBar />}
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path='/login' element={<Login/>} />
+        <Route path="/items" element={<ItemConsole />} />
+        <Route path="/requests" element={<RequestConsole />} />
+        
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/items" element={<ItemConsole />} />
-          <Route path="/requests" element={<RequestConsole />} />
-        </Routes>
+        <AppWrapper />
       </BrowserRouter>
     </div>
   );
