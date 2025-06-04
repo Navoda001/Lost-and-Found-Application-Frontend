6d3bf156ea25766f3206ec955c59523d2bda1480
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-06-04 22:31:55 +0530

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import ItemConsole from './components/Item/ItemConsole';
-import RequestConsole from './components/Request/RequestConsole';
+import Home from './components/Home';
 import SignUp from './components/Auth/Signup';
 import Login from './components/Auth/Login';
-import Home from './components/Home';
-import Profile from './components/Account/user/UserProfile';
-import { AuthProvider } from './components/Auth/AuthProvider';
+import ItemConsole from './components/Item/ItemConsole';
+import RequestConsole from './components/Request/RequestConsole';
+import UserProfile from './components/Account/user/UserProfile';
 import MyItemConsole from './components/Item/MyItems/MyItemConsole';
 import MyRequestConsole from './components/Request/MyRequest/MyRequestConsole';
 import StaffProfile from './components/Account/Staff/StaffProfile';
 import AdminProfile from './components/Account/Admin/AdminProfile';
-import CreateAccount from './components/Account/CreateAccount';
+import CreateAccounts from './components/Account/CreateAccount';
+import { AuthProvider } from './components/Auth/AuthProvider';
 
 
 
@@ -33,12 +33,12 @@ function AppWrapper() {
         <Route path='/login' element={<Login />} />
         <Route path="/items" element={<ItemConsole />} />
         <Route path="/requests" element={<RequestConsole />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<UserProfile />} />
         <Route path="/myitems" element={<MyItemConsole/>} />
         <Route path='/myrequests' element={<MyRequestConsole/>}/>
         <Route path='/staffprofile' element={<StaffProfile/>}/>
         <Route path='/adminprofile' element={<AdminProfile/>}/>
-        <Route path='/addaccounts' element={<CreateAccount/>}/>
+        <Route path='/addaccounts' element={<CreateAccounts/>}/>
 
 
       </Routes>
